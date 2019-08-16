5c62d4ba10bc29ec881f03a3a0f0c245f2969d0b
Marco <ma.monzon@soprasteria.com>
2019-08-16 14:57:20 +0200

    feat: click on dices trigers a function

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,10 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee 🎲'),
+          title: Text(
+            'Dicee 🎲',
+            style: TextStyle(letterSpacing: 2.5),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,18 +18,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice2.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                padding: EdgeInsets.all(0.0),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = 5;
+                  });
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                padding: EdgeInsets.all(0.0),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = 6;
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
